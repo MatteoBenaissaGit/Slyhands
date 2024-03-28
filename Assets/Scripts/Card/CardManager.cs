342692b0138871e46c76e73a6dd287aba4062378
P3D\simon.walenczak <simon.walenczak@lacatholille.fr>
2024-03-28 17:46:19 +0100

    [DEV] Setup Scriptable Card + Prefab Card Part 1

--- Assets/Scripts/Card/CardManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CardManager : MonoBehaviour
-{
-    public List<Card> deck = new List<Card>();
-    public Transform[] cardSlots;
-    public bool[] availableCardsSlots;
-}
