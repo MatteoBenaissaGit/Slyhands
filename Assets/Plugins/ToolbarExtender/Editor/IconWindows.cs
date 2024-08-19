6e402c33e44415c26999e6d021ee766b56e354a6
Mattéo Benaissa <matteobenaissa@gmail.com>
2024-08-20 01:22:46 +0200

    [DEV] icons

---
+++ Assets/Plugins/ToolbarExtender/Editor/IconWindows.cs
@@ -0,0 +1,90 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Plugins
+{
+    internal class IconWindows : EditorWindow
+    {
+        private string _selectedIconName = "";
+        private int _currentPage = 0;
+        private const int IconButtonSize = 50;
+        private string[] _guids;
+
+        [MenuItem("Tools/Icon Windows")]
+        public static void OpenWindow()
+        {
+            EditorWindow window = EditorWindow.GetWindow(typeof(IconWindows));
+        }
+
+        private void OnEnable()
+        {
+            _guids = AssetDatabase.FindAssets("", new[] { "Assets/Plugins/Icons/icons" });
+        }
+
+        private void OnGUI()
+        {
+            float windowWidth = position.width;
+            int columns = Mathf.FloorToInt(windowWidth / IconButtonSize) - 1;
+            float windowHeight = position.height;
+            int rows = Mathf.FloorToInt(windowHeight / IconButtonSize) - 2;
+            int iconsPerPage = rows * columns;
+
+            int totalPages = (_guids.Length + iconsPerPage - 1) / iconsPerPage;
+
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Previous") && _currentPage > 0)
+            {
+                _currentPage--;
+            }
+
+            if (GUILayout.Button("Next") && _currentPage < totalPages - 1)
+            {
+                _currentPage++;
+            }
+
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.LabelField($"Page {_currentPage + 1} of {totalPages}");
+
+            int start = _currentPage * iconsPerPage;
+            int end = Mathf.Min(start + iconsPerPage, _guids.Length);
+
+            EditorGUILayout.BeginVertical();
+            for (int i = start; i < end; i++)
+            {
+                string assetPath = AssetDatabase.GUIDToAssetPath(_guids[i]);
+                Texture icon = AssetDatabase.LoadAssetAtPath<Texture>(assetPath);
+                GUIContent content = new GUIContent(icon);
+
+                if (i % columns == 0)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                }
+
+                if (GUILayout.Button(content, GUILayout.Width(IconButtonSize), GUILayout.Height(IconButtonSize)))
+                {
+                    _selectedIconName = System.IO.Path.GetFileNameWithoutExtension(assetPath);
+                }
+
+                if (i % columns == columns - 1)
+                {
+                    EditorGUILayout.EndHorizontal();
+                }
+            }
+
+            EditorGUILayout.EndVertical();
+
+            if (string.IsNullOrEmpty(_selectedIconName) == false)
+            {
+                EditorGUILayout.BeginHorizontal();
+                if (GUILayout.Button(EditorGUIUtility.IconContent("d_winbtn_win_restore"), GUILayout.Width(30)))
+                {
+                    EditorGUIUtility.systemCopyBuffer = $"EditorGUIUtility.IconContent(\"{_selectedIconName}\")";
+                }
+
+                EditorGUILayout.LabelField(_selectedIconName);
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+    }
+}
--- Assets/Plugins/ToolbarExtender/Editor/ToolbarExtenderTools.cs
+++ Assets/Plugins/ToolbarExtender/Editor/ToolbarExtenderTools.cs
@@ -2,34 +2,39 @@
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityToolbarExtender;
 
-namespace Plugins.unity_toolbar_extender_master.Editor
+namespace Plugins
 {
     [InitializeOnLoad]
-    public class SceneSwitchLeftButton
+    public class ScenePanelToolbar
     {
         private static SceneList _sceneList;
-        
-        static SceneSwitchLeftButton()
+
+        static ScenePanelToolbar()
         {
-            ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+            UnityToolbarExtender.ToolbarExtender.LeftToolbarGUI.Add(OnScenePanelGUI);
         }
 
-        private static void OnToolbarGUI()
+        private static void OnScenePanelGUI()
         {
             GUILayout.FlexibleSpace();
 
-            if(GUILayout.Button(new GUIContent("GameScene", "Start GameScene")))
+            if (GUILayout.Button(new GUIContent("GameScene", "Start GameScene")))
             {
-                EditorSceneManager.OpenScene("Assets/Scenes/GameScene.unity");
+                if (Event.current.button == 0)
+                {
+                    EditorSceneManager.OpenScene("Assets/Scenes/GameScene.unity");
+                }
             }
 
-            if(GUILayout.Button(new GUIContent("LevelEditorScene", "Start LevelEditorScene")))
+            if (GUILayout.Button(new GUIContent("LevelEditorScene", "Start LevelEditorScene")))
             {
-                EditorSceneManager.OpenScene("Assets/Scenes/LevelEditorScene.unity");
+                if (Event.current.button == 0)
+                {
+                    EditorSceneManager.OpenScene("Assets/Scenes/LevelEditorScene.unity");
+                }
             }
-            
+
             DraggableSceneSpace();
         }
 
@@ -48,12 +53,17 @@ namespace Plugins.unity_toolbar_extender_master.Editor
 
             _sceneList.Scenes.RemoveAll(x => x == null);
 
+            if (_sceneList.Scenes.Count > 0)
+            {
+                EditorGUILayout.Space(20);
+            }
+
             //draw scenes
             List<int> scenesToRemove = new();
             for (int i = 0; i < _sceneList.Scenes.Count; i++)
             {
                 EditorGUILayout.BeginHorizontal();
-                
+
                 if (GUILayout.Button(_sceneList.Scenes[i].name))
                 {
                     if (Event.current.button == 0)
@@ -65,9 +75,10 @@ namespace Plugins.unity_toolbar_extender_master.Editor
                         scenesToRemove.Add(i);
                     }
                 }
-                
+
                 EditorGUILayout.EndHorizontal();
             }
+
             foreach (int index in scenesToRemove)
             {
                 _sceneList.Scenes.RemoveAt(index);
@@ -75,12 +86,46 @@ namespace Plugins.unity_toolbar_extender_master.Editor
 
             // draggable space
             EditorGUILayout.Space(20);
-            SceneAsset newScene = (SceneAsset)EditorGUILayout.ObjectField(null, typeof(SceneAsset), false);
+            SceneAsset newScene = (SceneAsset)EditorGUILayout.ObjectField(null, typeof(SceneAsset), false, GUILayout.Width(150));
+       
             if (newScene != null)
             {
                 _sceneList.Scenes.Add(newScene);
                 EditorUtility.SetDirty(_sceneList);
                 AssetDatabase.SaveAssetIfDirty(_sceneList);
+            }
+        }
+
+        [InitializeOnLoad]
+        public class ToolbarRightButtons
+        {
+            static ToolbarRightButtons()
+            {
+                UnityToolbarExtender.ToolbarExtender.RightToolbarGUI.Add(OnButtonsGUI);
+            }
+
+            private static void OnButtonsGUI()
+            {
+                GUILayout.FlexibleSpace();
+            
+                if (GUILayout.Button(new GUIContent(EditorGUIUtility.IconContent("CustomTool"))))
+                {
+                    Plugins.IconWindows.OpenWindow();
+                }
+            
+                EditorGUILayout.Space(30);
+            
+                if (GUILayout.Button(new GUIContent(EditorGUIUtility.IconContent("d_Settings"))))
+                {
+                    SettingsService.OpenProjectSettings("Project/Player");
+                }
+            
+                if (GUILayout.Button(new GUIContent(EditorGUIUtility.IconContent("d_Settings"))))
+                {
+                    SettingsService.OpenUserPreferences();
+                }
+            
+                EditorGUILayout.Space(30);
             }
         }
     }
