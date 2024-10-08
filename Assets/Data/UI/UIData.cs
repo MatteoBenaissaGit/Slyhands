079069155df6bcca01f3c215367c4b3eea65cf4b
Matteo Benaissa <matteobenaissa@gmail.com>
2024-10-08 16:08:41 +0200

    [DEV] UI Data

---
+++ Assets/Data/UI/UIData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Data.UIElements
+{
+    [Serializable]
+    public struct UIElementData
+    {
+
+        [field: SerializeReference] [field: TableColumnWidth(57, Resizable = false)] [field: PreviewField(Alignment = ObjectFieldAlignment.Center)]
+        public Sprite Icon { get; set; }
+        
+        [field:SerializeField] public string Id { get; set; }
+    }
+    
+    [CreateAssetMenu(fileName = "Data", menuName = "Data/UI", order = 2)]
+    public class UIData : ScriptableObject
+    {
+        [TableList(AlwaysExpanded = true, DrawScrollView = false)]
+        [SerializeField] private List<UIElementData> _spritesData = new List<UIElementData>();
+        
+        public Sprite GetSprite(string id)
+        { 
+            Sprite sprite = _spritesData.Find(x => x.Id == id).Icon;
+            return sprite;
+        }
+    }
+}
